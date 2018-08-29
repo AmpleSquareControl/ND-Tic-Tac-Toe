79fb3198a1ed7fcf0c32252f6873b2ace59419a4
John Hughes <johnlodgerhughes@gmail.com>
2018-08-28 23:42:17 -0400

    Changed data structures to make run-time of gameplay itself minimal

--- faster_game.py
+++ faster_game.py
@@ -3,49 +3,74 @@ import sys
 from operator import add
 
 """
-Creates 2 data structures:
-1. List of all valid tic-tac-toe lines
-2. Dictionary mapping each line to the player that controls it (0 if nobody does)
+Produces and returns all valid tic-tac-toe lines for a board size
 
-@return: Above data structures
+@return: List of sets. Each set contains the coordinates of a line
 """
-def makeLineDict():
-	lineControllers = {}
-	axisOrder = ()
-	for i in range(d):
-		axisOrder = axisOrder + (i,)
+def getAllLines():
+	lines = []
 
-	#rows and columns
+	#straight lines along a dimension
 	coords = np.empty(shape, dtype=object)
 	for x,_ in np.ndenumerate(board):
 		coords[x] = x
+
+	axisOrder = ()
+	for i in range(d):
+		axisOrder = axisOrder + (i,)
 
 	for i in range(d):
 		axisOrder = axisOrder[1:] + (axisOrder[0],)
 		transposedCoords = coords.transpose(axisOrder)
 		transposedCoords = transposedCoords.reshape(n**(d-1),n)
 		for j in range(len(transposedCoords)):
-			lineControllers[tuple(transposedCoords[j])] = 0
+			lines.append(set(transposedCoords[j]))
 
-	#diagonals
-	cornerList = createCornerList(n, d) #Only corners with first coord = 0 to avoid double counting
-	for i in range(len(cornerList)):
-		square = cornerList[i]
-		increments = () #direction of hypercube diagonal associated with this corner
-		for j in range(d):
-			if square[j] == 0:
-				increments += (1,)
-			else:
-				increments += (-1,)
+	"""
+	Diagonals for subspace of dimension d. coordIndList is a list of indices to add
+	coordinates from the original hypercube space. coordList contains the values to add
+	at these indices
+	"""
+	def getDiagonals(subD, coordIndList, coordList):
+		if subD == 1:
+			return
 
-		diagonal = (square,)
-		for _ in range(n-1):
-			square = tuple(map(add, square, increments))
-			diagonal += (square,)
+		cornerList = createCornerList(n, subD) #Only corners with first coord = 0 to avoid double counting
+		for i in range(len(cornerList)):
+			subSquare = cornerList[i]
+			increments = () #direction of hypercube diagonal associated with this corner
+			for j in range(subD):
+				if subSquare[j] == 0:
+					increments += (1,)
+				else:
+					increments += (-1,)
 
-		lineControllers[diagonal] = 0
+			subDiagonal = set()
+			for _ in range(n):
+				subDiagonal.add(subSquare)
+				subSquare = tuple(map(add, subSquare, increments))
 
-	return (list(lineControllers.keys()),lineControllers)
+			diagonal = set()
+			for subSquare in subDiagonal:
+				ss = list(subSquare)
+				for j in range(len(coordList)):
+					ss.insert(coordIndList[j], coordList[j])
+				diagonal.add(tuple(ss))
+
+			lines.append(diagonal)
+
+		#Get diagonals in subspaces
+		lastInd = -1
+		if len(coordIndList) > 0:
+			lastInd = coordIndList[-1]
+
+		if subD > 2:
+			for i in range(lastInd+1,d):
+				for j in range(n):
+					getDiagonals(subD-1,coordIndList + [i],coordList + [j])
+
+	getDiagonals(d, [], [])
+	return lines
 
 """
 Creates a mapping from each square on the board to the indices of the
@@ -88,5 +113,7 @@ if __name__ == '__main__':
 	for i in range(d):
 		shape = shape + (n,)
 	board = np.zeros(shape, dtype=int)
-	lines,lineControllers = makeLineDict()
+	lines = getAllLines()
 	linesForSquare = createSquareToLineMapping()
+	lineControllers = np.zeros(len(lines)) #Player number that controls a line, 0 if no markers
+	movesOnLine = np.zeros(len(lines)) #Number of pieces a player has on a line
